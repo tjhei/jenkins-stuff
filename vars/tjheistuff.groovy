a1ec6df30285774c3070ae36419616b86938b2e3
Timo Heister <timo.heister@gmail.com>
2019-03-24 17:22:48 -0600

    add file

---
+++ vars/tjheistuff.groovy
@@ -0,0 +1,10 @@
+import hudson.model.Result
+import jenkins.model.CauseOfInterruption.UserInterruption
+
+// from https://stackoverflow.com/questions/40760716/jenkins-abort-running-build-if-new-one-is-started
+
+def killOldBuilds() {
+  while(currentBuild.rawBuild.getPreviousBuildInProgress() != null) {
+    currentBuild.rawBuild.getPreviousBuildInProgress().doKill()
+  }
+}
