b87a6eb6039f8f16870ae91bf62429e5bec8e6a3
Timo Heister <timo.heister@gmail.com>
2019-03-25 08:57:27 -0600

    other variant

--- vars/killold.groovy
+++ vars/killold.groovy
@@ -1,4 +1,5 @@
 import hudson.model.Result
+import hudson.model.Run
 import jenkins.model.CauseOfInterruption.UserInterruption
 
 // from https://stackoverflow.com/questions/40760716/jenkins-abort-running-build-if-new-one-is-started
@@ -8,4 +9,24 @@ def killOldBuilds() {
     currentBuild.rawBuild.getPreviousBuildInProgress().doTerm()
     //.doKill()
   }
+}
+
+
+
+def abortPreviousBuilds() {
+    Run previousBuild = currentBuild.rawBuild.getPreviousBuildInProgress()
+
+    while (previousBuild != null) {
+        if (previousBuild.isInProgress()) {
+            def executor = previousBuild.getExecutor()
+            if (executor != null) {
+                echo ">> Aborting older build #${previousBuild.number}"
+                executor.interrupt(Result.ABORTED, new UserInterruption(
+                    "Aborted by newer build #${currentBuild.number}"
+                ))
+            }
+        }
+
+        previousBuild = previousBuild.getPreviousBuildInProgress()
+    }
 }
